9c018e4cfae74a31fa58127f79ded0a4f01bb3be
Antee <Antee@Antees-MBP.setup.net>
2018-12-02 22:24:27 +0800

    add indent config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,6 @@
 (require 'package)
 
-(add-to-list 'package-archives '("MELPA Stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("MELPA Stable" . "https://stable.melpa.org/packages/"))
 (package-initialize)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (auto-complete neotree yasnippet use-package flycheck))))
+    (no-littering auto-complete neotree yasnippet use-package flycheck))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -19,6 +19,8 @@
 
 ;; enable functionalities
 ;; common
+(setq c-default-style "linux"
+          c-basic-offset 2)
 (global-linum-mode t)
 
 ;; flycheck
